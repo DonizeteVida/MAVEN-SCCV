f9d886fcf930ef560f90848a44b6664242f783cc
DonizeteVida <donizetevida1998@outlook.cm>
2018-09-14 10:41:24 -0300

    aa
    aa

--- src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AlunoCadastroCurriculoMBean.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AlunoCadastroCurriculoMBean.java
@@ -1,6 +1,8 @@
 package br.senai.sp.jaguariuna.sccv.mBeans;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
@@ -11,6 +13,8 @@ import javax.faces.context.FacesContext;
 
 import br.senai.sp.jaguariuna.sccv.entities.CurriculumVitae;
 import br.senai.sp.jaguariuna.sccv.entities.Usuario;
+import br.senai.sp.jaguariuna.sccv.subEntities.ClasseGenerica;
+import br.senai.sp.jaguariuna.sccv.uDao.ClasseGenericaDao;
 import br.senai.sp.jaguariuna.sccv.uDao.UsuarioDao;
 
 @ManagedBean
@@ -20,10 +24,22 @@ public class AlunoCadastroCurriculoMBean {
 	private Usuario usuario;
 	private UsuarioDao usuarioDao;
 	private CurriculumVitae curriculumVitae;
+	private List<ClasseGenerica> cursos;
+	private List<ClasseGenerica> turmas;
+
+	private ClasseGenericaDao classeGenericaDao;
 
 	public AlunoCadastroCurriculoMBean() {
+		turmas = new ArrayList<>();
 		usuarioDao = new UsuarioDao();
 		curriculumVitae = new CurriculumVitae();
+		classeGenericaDao = new ClasseGenericaDao();
+		try {
+			cursos = classeGenericaDao.buscaCurso();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	@PostConstruct
@@ -35,6 +51,15 @@ public class AlunoCadastroCurriculoMBean {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			mens(e.toString());
+		}
+	}
+
+	public void buscaTurma() {
+		try {
+			turmas = classeGenericaDao.buscaTurma(curriculumVitae.getCurso().getId());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
@@ -63,6 +88,22 @@ public class AlunoCadastroCurriculoMBean {
 
 	public void setCurriculumVitae(CurriculumVitae curriculumVitae) {
 		this.curriculumVitae = curriculumVitae;
+	}
+
+	public List<ClasseGenerica> getCursos() {
+		return cursos;
+	}
+
+	public void setCursos(List<ClasseGenerica> cursos) {
+		this.cursos = cursos;
+	}
+
+	public List<ClasseGenerica> getTurmas() {
+		return turmas;
+	}
+
+	public void setTurmas(List<ClasseGenerica> turmas) {
+		this.turmas = turmas;
 	}
 
 }
