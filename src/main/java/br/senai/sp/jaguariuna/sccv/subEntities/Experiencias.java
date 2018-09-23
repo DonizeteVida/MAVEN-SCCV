cf537ef7c3ef846954cf18522495a4c70c08529d
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2018-09-23 00:35:56 -0300

    a
    a

--- src/main/java/br/senai/sp/jaguariuna/sccv/subEntities/Experiencias.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/subEntities/Experiencias.java
@@ -3,33 +3,49 @@ package br.senai.sp.jaguariuna.sccv.subEntities;
 import java.util.Calendar;
 
 public class Experiencias {
-	
+
 	private Integer id;
 	private String nome;
 	private Calendar data_inicio;
 	private Calendar data_fim;
-	
+	private Integer id_curriculum_vitae;
+
+	public Integer getId_curriculum_vitae() {
+		return id_curriculum_vitae;
+	}
+
+	public void setId_curriculum_vitae(Integer id_curriculum_vitae) {
+		this.id_curriculum_vitae = id_curriculum_vitae;
+	}
+
 	public Integer getId() {
 		return id;
 	}
+
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Calendar getData_inicio() {
 		return data_inicio;
 	}
+
 	public void setData_inicio(Calendar data_inicio) {
 		this.data_inicio = data_inicio;
 	}
+
 	public Calendar getData_fim() {
 		return data_fim;
 	}
+
 	public void setData_fim(Calendar data_fim) {
 		this.data_fim = data_fim;
 	}
