2cd5aaadfec4edc5163e1d8cc3639f218a944305
DonizeteVida <donizetevida1998@outlook.cm>
2018-10-01 08:52:50 -0300

    a
    a

--- src/main/java/br/senai/sp/jaguariuna/sccv/uDao/AdministradorDao.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/uDao/AdministradorDao.java
@@ -10,46 +10,43 @@ import br.senai.sp.jaguariuna.sccv.jdbc.ConnectionDB;
 import br.senai.sp.jaguariuna.sccv.utils.StringToMD5;
 import br.senai.sp.jaguariuna.sccv.entities.UsuarioAdministrador;
 
-
 public class AdministradorDao {
 
 	private Connection conn;
-	
+
 	public AdministradorDao() {
 		conn = ConnectionDB.getConnection();
 	}
-	
-	public boolean updateUsuarioAdministrador (UsuarioAdministrador a ) throws SQLException {
-		 
+
+	public boolean updateUsuarioAdministrador(UsuarioAdministrador a) throws SQLException {
+
 		String sql = "UPDATE usuario SET nome = ?, email = ?, senha = ?, nif = ?, id_status = ?, WHERE usuario.id = ?;";
 
 		PreparedStatement ps = conn.prepareStatement(sql);
-		
+
 		ps.setString(1, a.getNome());
 		ps.setString(2, a.getEmail());
 		ps.setString(3, StringToMD5.convertStringToMd5(a.getSenha()));
-		ps.setString(4,a.getNif() );
+		ps.setString(4, a.getNif());
 		ps.setInt(5, a.getStatus().getId());
-		
+
 		return ps.executeUpdate() > 0;
 	}
-	
-	public boolean inserirUsuarioAdministrador(UsuarioAdministrador a) throws SQLException{
-		String sql = "INSERT INTO usuario(nome, email, senha, idade, nif, status)"
-				+ " VALUES(?,?,?,?,?,?)";
-		
+
+	public boolean inserirUsuarioAdministrador(UsuarioAdministrador a) throws SQLException {
+		String sql = "INSERT INTO usuario(nome, email, senha, idade, nif, status)" + " VALUES(?,?,?,?,?,?)";
+
 		PreparedStatement ps = conn.prepareStatement(sql);
-		
+
 		ps.setString(1, a.getNome());
 		ps.setString(2, a.getEmail());
 		ps.setString(3, StringToMD5.convertStringToMd5(a.getSenha()));
-		ps.setString(4,a.getNif() );
+		ps.setString(4, a.getNif());
 		ps.setInt(5, a.getStatus().getId());
-		
+
 		return ps.executeUpdate() > 0;
 	};
-	
-<<<<<<< HEAD
+
 //	public AdministradorDao buscarAdministradorPorNif(String nif) throws SQLException{
 //		String sql = "SELECT tudo = ?, WHERE a.nif = ?;";
 //				
@@ -71,30 +68,26 @@ public class AdministradorDao {
 //		
 //		
 //	}
-=======
-	public AdministradorDao buscarAdministradorPorNif(String nif) throws SQLException{
+
+	public AdministradorDao buscarAdministradorPorNif(String nif) throws SQLException {
 		String sql = "SELECT tudo = ?, WHERE a.nif = ?;";
-				
-		PreparedStatement ps = 	conn.prepareStatement(sql);
-		//ps.setString(4,getNif(nif));
-		
+
+		PreparedStatement ps = conn.prepareStatement(sql);
+		// ps.setString(4,getNif(nif));
+
 		ResultSet rs = ps.executeQuery();
-		
+
 		AdministradorDao a = null;
-		if(rs.next()) {
+		if (rs.next()) {
 			a = new AdministradorDao();
-			
+
 			Calendar c = Calendar.getInstance();
 			c.setTimeInMillis(rs.getLong("idade"));
-			
-			
-		}
-		
-		return a;
-		
-	}
->>>>>>> 2cff5412cee05ed6d5f11f7eaa73586692133dd6
-	
 
-	
+		}
+
+		return a;
+
+	}
+
 }
