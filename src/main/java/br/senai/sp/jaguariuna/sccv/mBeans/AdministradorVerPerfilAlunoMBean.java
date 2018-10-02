aef09590f147f7ccf436e3c6502ab92ff60112c6
SenaiNailton <42775166+SenaiNailton@users.noreply.github.com>
2018-10-02 07:50:17 -0300

    Commit tela Admin Visualização de alunos (jose)

--- src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorVerPerfilAlunoMBean.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorVerPerfilAlunoMBean.java
@@ -1,10 +1,42 @@
 package br.senai.sp.jaguariuna.sccv.mBeans;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
-@ManagedBean
+import br.senai.sp.jaguariuna.sccv.entities.Usuario;
+import br.senai.sp.jaguariuna.sccv.uDao.UsuarioDao;
+import br.senai.sp.jaguariuna.sccv.utils.Mensagem;
+
+@ManagedBean(eager = true)
 @ViewScoped
 public class AdministradorVerPerfilAlunoMBean {
-	
+
+	UsuarioDao usuarioDao;
+	List<Usuario> listaUsuario;
+
+	public AdministradorVerPerfilAlunoMBean() {
+		usuarioDao = new UsuarioDao();
+		listaUsuario = new ArrayList<Usuario>();
+
+		try {
+			listaUsuario = usuarioDao.listarUsuario();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Mensagem.make(e.toString());
+		}
+	}
+
+	public List<Usuario> getListaUsuario() {
+		return listaUsuario;
+	}
+
+	public void setListaUsuario(List<Usuario> listaUsuario) {
+		this.listaUsuario = listaUsuario;
+	}
+
 }
