fc1f9ad2d403a5433fa6cb3abaedf053b3bf3ce5
SenaiNailton <42775166+SenaiNailton@users.noreply.github.com>
2018-10-05 06:19:20 -0300

    m
    m

---
+++ src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/GraficoMBean.java
@@ -0,0 +1,53 @@
+package br.senai.sp.jaguariuna.sccv.mBeans;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import org.primefaces.model.chart.PieChartModel;
+
+@ManagedBean
+@ViewScoped
+public class GraficoMBean {
+
+	private PieChartModel grafico;
+
+	@PostConstruct
+	public void init() {
+		createGrafico();
+	}
+
+	public PieChartModel getGrafico() {
+		return grafico;
+	}
+
+	public void setGrafico(PieChartModel grafico) {
+		this.grafico = grafico;
+	}
+
+	public void createGrafico() {
+		createPieModel();
+	}
+
+	public void createPieModel() {
+
+		grafico = new PieChartModel();
+
+		Map<String, Number> dados = new HashMap<>();
+
+		dados.put("Técnico em informática", 500);
+		dados.put("Técnico em eletrônica", 500);
+
+		grafico.setData(dados);
+
+		grafico.setTitle("Curriclos cadastrados");
+		grafico.setLegendPosition("e");
+		grafico.setFill(true);
+		grafico.setShowDataLabels(true);
+		grafico.setShadow(false);
+		grafico.setDiameter(200);
+
+	}
+}
