c944e922c4423ad876c8ec9fff649c56b6be74eb
SenaiNailton <42775166+SenaiNailton@users.noreply.github.com>
2018-10-16 08:29:34 -0300

    a
    a

--- src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorEditarAlunoMBean.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorEditarAlunoMBean.java
@@ -1,5 +1,6 @@
 package br.senai.sp.jaguariuna.sccv.mBeans;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,6 +12,7 @@ import javax.faces.bean.ViewScoped;
 import br.senai.sp.jaguariuna.sccv.entities.Usuario;
 import br.senai.sp.jaguariuna.sccv.uDao.AdministradorDao;
 import br.senai.sp.jaguariuna.sccv.uDao.UsuarioDao;
+import br.senai.sp.jaguariuna.sccv.utils.Mensagem;
 
 @ManagedBean
 @ViewScoped
@@ -30,7 +32,18 @@ public class AdministradorEditarAlunoMBean {
 
 	@PostConstruct
 	void postConstruct() {
-		usuarioSelecionado = administradorVerPerfilAlunoMBean.getUsuarioSelecionado();
+		try {
+			downloadUsuario();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Mensagem.make(e.toString());
+		}
+	}
+
+	private void downloadUsuario() throws SQLException {
+		usuarioSelecionado = usuarioDao
+				.buscaUsuarioPorCpf(administradorVerPerfilAlunoMBean.getUsuarioSelecionado().getCpf());
 	}
 
 	@ManagedProperty(value = "#{administradorVerPerfilAlunoMBean}")
@@ -70,6 +83,18 @@ public class AdministradorEditarAlunoMBean {
 
 	public void setUsuario(List<Usuario> usuario) {
 		this.usuario = usuario;
+	}
+
+	public void updateUsuario() {
+		try {
+			if (usuarioDao.updateUsuario(usuarioSelecionado)) {
+				downloadUsuario();
+				administradorVerPerfilAlunoMBean.atualizaListaUsuario();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
--- src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorVerPerfilAlunoMBean.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/mBeans/AdministradorVerPerfilAlunoMBean.java
@@ -26,7 +26,6 @@ public class AdministradorVerPerfilAlunoMBean {
 		usuarioSelecionado = new Usuario();
 
 		atualizaListaUsuario();
-
 	}
 
 	public void buscaFiltro() {
