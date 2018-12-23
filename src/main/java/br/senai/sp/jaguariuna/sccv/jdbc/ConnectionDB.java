1016bc6bc20ead50864396844f1c215433977847
DonizeteVida <41035417+DonizeteVida@users.noreply.github.com>
2018-12-23 00:14:08 -0200

    a
    a

--- src/main/java/br/senai/sp/jaguariuna/sccv/jdbc/ConnectionDB.java
+++ src/main/java/br/senai/sp/jaguariuna/sccv/jdbc/ConnectionDB.java
@@ -6,9 +6,9 @@ import java.sql.SQLException;
 
 public class ConnectionDB {
 	private static Connection con;
-	private static String dns = "jdbc:mysql://localhost:3306/SCCV?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+	private static String dns = "jdbc:mysql://127.0.0.1:3306/SCCV?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
 	private static String user = "root";
-	private static String pass = "";
+	private static String pass = "root";
 
 	public static Connection getConnection() {
 		if (con == null) {
@@ -27,7 +27,7 @@ public class ConnectionDB {
 
 	private static void getGet() {
 		try {
-			DriverManager.registerDriver(new com.mysql.cj.jdbc.Driver());
+			DriverManager.registerDriver(new org.mariadb.jdbc.Driver());
 			con = DriverManager.getConnection(dns, user, pass);
 			System.out.println("DB conectado com sucesso!");
 		} catch (SQLException e) {
